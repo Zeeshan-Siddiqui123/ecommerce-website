de927e62e8e16c2b309bb878992bfe7700d28cb4
Zeeshan Siddiqui <155655770+Zeeshan-Siddiqui123@users.noreply.github.com>
2025-01-01 12:47:10 +0500

    Update Card.jsx

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,4 +1,4 @@
-import t, { useContext } from 'react';
+import React, { useContext } from 'react';
 import { MdOutlineShoppingCart } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../Screens/CartContext';
